34e40448d3c4326faca1c00565e0bf790a584363
Stewart ilondanga <stilondanga@gmail.com>
2019-08-16 14:40:59 +0300

    add gradle.properties

--- app/src/main/java/stewilondanga/github/io/lifecycles/MainActivity.java
+++ app/src/main/java/stewilondanga/github/io/lifecycles/MainActivity.java
@@ -1,13 +1,6 @@
 package stewilondanga.github.io.lifecycles;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
