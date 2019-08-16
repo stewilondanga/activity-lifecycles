1fc90b10f09936fc96f5f3b72be5506c51088471
Stewart ilondanga <stilondanga@gmail.com>
2019-08-16 15:53:21 +0300

    add app/src/main/java/stewilondanga/github/io/lifecycles/MainActivity.java

--- app/src/main/java/stewilondanga/github/io/lifecycles/MainActivity.java
+++ app/src/main/java/stewilondanga/github/io/lifecycles/MainActivity.java
@@ -20,4 +20,29 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         Log.d(tag, "In the onStart() event");
     }
+    public void onRestart ()
+    {
+        super.onRestart();
+        Log.d(tag, "In the onRestart() event");
+    }
+    public void onResume ()
+    {
+        super.onResume();
+        Log.d(tag, "In the onResume() event");
+    }
+    public void onPause ()
+    {
+        super.onPause();
+        Log.d(tag, "In the onPause() event");
+    }
+    public void onStop ()
+    {
+        super.onStop();
+        Log.d(tag, "In the onStop() event");
+    }
+    public void onDestroy ()
+    {
+        super.onDestroy();
+        Log.d(tag, "In the onDestroy() event");
+    }
 }
