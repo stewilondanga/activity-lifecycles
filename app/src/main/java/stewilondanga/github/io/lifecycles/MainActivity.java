47035cd7d42af098b96f1f1f234d3350fe86d4c4
Stewart ilondanga <stilondanga@gmail.com>
2019-08-16 15:37:05 +0300

    add app/src/main/java/stewilondanga/github/io/lifecycles/MainActivity.java

--- app/src/main/java/stewilondanga/github/io/lifecycles/MainActivity.java
+++ app/src/main/java/stewilondanga/github/io/lifecycles/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    String tag = "LifeCycleEvents";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
