6682e723bdce3f232c5ab2a87d354c0495efad00
Stewart ilondanga <stilondanga@gmail.com>
2019-08-16 16:00:57 +0300

    add app/src/main/java/stewilondanga/github/io/lifecycles/MainActivity.java

--- app/src/main/java/stewilondanga/github/io/lifecycles/MainActivity.java
+++ app/src/main/java/stewilondanga/github/io/lifecycles/MainActivity.java
@@ -45,4 +45,11 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Log.d(tag, "In the onDestroy() event");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        //Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater() .inflate(R.menu.menu_main, menu);
+        return true;
+    }
 }
